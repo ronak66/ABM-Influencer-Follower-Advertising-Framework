d0a80d56ee58c06223c1fe23b406ae1de177b690
ronak66 <ronakdoshi36@gmail.com>
2020-08-18 16:15:32 +0530

    Mesa: Adding tutorials

--- mesa_tutorial/money_model.py
+++ mesa_tutorial/money_model.py
@@ -13,7 +13,20 @@ class MoneyAgent(Agent):
         self.wealth = 1
         self.start = start
 
+    # def move(self):
+    #     possible_steps = self.model.grid.get_neighborhood(
+    #         self.pos,
+    #         moore=True,
+    #         include_center=False)
+    #     new_position = self.random.choice(possible_steps)
+    #     self.model.grid.move_agent(self, new_position)
+
     def give_money(self):
+        # cellmates = self.model.grid.get_cell_list_contents([self.pos])
+        # if len(cellmates) > 1:
+        #     other_agent = self.random.choice(cellmates)
+        #     other_agent.wealth += 1
+        #     self.wealth -= 1
         self.wealth-=1
 
     def step(self):
@@ -21,11 +34,20 @@ class MoneyAgent(Agent):
         if self.wealth > 0:
             self.give_money()
 
+
+def compute_gini(model):
+    agent_wealths = [agent.wealth for agent in model.schedule.agents]
+    x = sorted(agent_wealths)
+    N = model.num_agents
+    B = sum( xi * (N-i) for i,xi in enumerate(x) ) / (N*sum(x))
+    return (1 + (1/N) - 2*B)
+
 class MoneyModel(Model):
     """A model with some number of agents."""
     def __init__(self, N, width, height):
         self.num_agents = N
         self.grid = MultiGrid(width, height, True)
+        # self.schedule = RandomActivation(self)
         self.running = True
         self.agents = []
 
@@ -38,18 +60,31 @@ class MoneyModel(Model):
             else: start = 0
 
             a = MoneyAgent(i, self, start)
+            # self.schedule.add(a)
             # Add the agent to a random grid cell
             x = self.random.randrange(self.grid.width)
             y = self.random.randrange(self.grid.height)
+            # print(i,j)
             self.grid.place_agent(a, (k, j))
             if(j==9):
                 j=0
                 k+=1
             else:
                 j+=1
+            # if(j==self.grid.height-1):
+            #     i+=1
+            #     j=0
+            # else:
+            #     j+=1
             self.agents.append(a)
 
+        # self.datacollector = DataCollector(
+        #     model_reporters={"Gini": compute_gini},
+        #     agent_reporters={"Wealth": "wealth"})
+
     def step(self):
+        # self.datacollector.collect(self)
+        # self.schedule.step()
         x = random.choice(range(len(self.agents)))
         if(self.agents[x].start != 1):
             self.agents[x].step()
